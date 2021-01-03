b63c7c19230e52a83147aacdecb43d95596d1b90
Bernd <bernd.rode@gmail.com>
2021-01-03 17:18:27 +0100

    Add missing changes

--- src/org/berosoft/apps/BeroView/BeroViewExplorer.java
+++ src/org/berosoft/apps/BeroView/BeroViewExplorer.java
@@ -7,7 +7,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.PrintWriter;
 import java.util.*;
 
 import javax.swing.*;
